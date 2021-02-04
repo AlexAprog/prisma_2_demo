f6c1c392a399100f2cb0a46c6d314cc56aa25691
AlexAprog <55787742+AlexAprog@users.noreply.github.com>
2021-02-04 11:38:30 +0300

    add graphql server

---
+++ pages/api/graphql.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { makeExecutableSchema } from 'graphql-tools';
+import Cors from 'micro-cors';
+import { typeDefs } from '../../utils/api/typeDefs';
+import { resolvers } from '../../utils/api/resolvers';
+
+const cors = Cors();
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+const handler = new ApolloServer({
+	schema,
+}).createHandler({
+	path: '/api/graphql',
+});
+
+export default cors((req, res) => {
+	return handler(req, res);
+});
